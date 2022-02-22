e98ff47cea9e60ee62e266101a83ac23353fe8d5
Pratik Kore <pratikkore5051@gmail.com>
2022-02-22 16:51:08 +0530

    react initial

---
+++ Day-4-ReactJs-code-content/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Display7 from './Display7';
+
+function App() {
+  return (
+    <>
+    {/* Hellow */}
+      <Display7/>
+    </>
+  );
+}
+export default App;
+
